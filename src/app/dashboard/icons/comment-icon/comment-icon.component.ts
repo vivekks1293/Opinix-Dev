6289125663e07425ba04279d562e1c45e2d3c61f
vivekks1293 <vivekks1293@gmail.com>
2025-04-05 18:29:52 +0530

    Footer header fixes

---
+++ src/app/dashboard/icons/comment-icon/comment-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'comment-icon',
+  standalone: false,
+  templateUrl: './comment-icon.component.html',
+  styleUrl: './comment-icon.component.css'
+})
+export class CommentIconComponent {
+
+}
